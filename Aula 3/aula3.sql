5a0207033b8ec0f7a006c43d3260cd9d35026cc1
Bismarck <brunowasborn2rock@gmail.com>
2024-12-31 14:54:14 -0300

    CREATE

---
+++ Aula 3/aula3.sql
@@ -0,0 +1,12 @@
+create schema cfbcursos;
+create database cfbcursos;
+-- Os dois são iguais
+-- CTRL+Espaço mostra opções
+
+create table cliente(
+	-- nomenclatura: tipo_coluna_tabela
+	i_cliente_cliente int primary key auto_increment,
+    s_nomecli_cliente varchar(50) not null,
+    s_cpf_cliente varchar(50) not null,
+    d_nasc_cliente datetime
+);
