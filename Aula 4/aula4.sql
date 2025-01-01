becc9be6553b6a0417f18cc0b1dd5254d20268ba
Bismarck <brunowasborn2rock@gmail.com>
2025-01-01 15:43:04 -0300

    alter drop

--- Aula 4/aula4.sql
+++ Aula 4/aula4.sql
@@ -12,3 +12,12 @@ create table cliente(
     d_nasc_cliente datetime
 );
 
+
+DROP TABLE cliente; -- deleta a tabela
+DROP SCHEMA cfbcursos; -- deleta o banco de dados
+
+ALTER TABLE cliente MODIFY COLUMN s_nomecli_cliente VARCHAR(30);
+
+ALTER TABLE cliente add i_tipo_cliente INT default 1;
+ALTER TABLE cliente drop column i_tipo_cliente;
+ALTER TABLE cliente add i_tipo_cliente INT NOT NULL;
