5e25e4abcfc0dd5caae657d1c083a0f07d7697db
Bismarck <brunowasborn2rock@gmail.com>
2025-01-04 15:02:20 -0300

    ok

--- Aula 5/aula5.sql
+++ Aula 5/aula5.sql
@@ -1,8 +1,43 @@
-CREATE TABLE tipocliente(
-	i_tipocliente_tipocliente INT primary key not null,
-    s_dsctipocliente_tipocliente VARCHAR(100) not null
-);
+-- tipos de dados mais comuns no sql
 
--- alter table NOMETABELA add constraint NOMECONSTRAINT foreign key (campo da tabela origem) references TABELADESTINO (campo da tabela destino)
+/*
+TEXTO
 
- alter table cliente add constraint fk_tipocliente foreign key (i_tipo_cliente) references tipocliente (i_tipocliente_tipocliente);
+tinytext - até 255 bytes
+text - até 65.535 bytes
+mediumtext - até 16.777.215 bytes
+longtext - até 4.294.967.295 bytes
+
+char (caracteres) - 2  55 bytes fixo
+varchar (caracteres) - até 65.535 bytes, tamanho variável
+*/
+
+
+/*
+NÚMERO
+tinyint
+smallint
+mediumint
+int
+bigint
+
+float (precisão, escala)
+double (precisão, escala)
+decimal (precisão, escala)
+
+*/
+
+
+/*
+TEMPORAIS
+
+date - AAAA-MM-DD
+datetime - AAAA-MM-DD HH:MI:SS
+timestamp - AAAA-MM-DD HH:MI:SS mas 
+year - 1901 a 2155
+time - HH:MI:SS
+
+*/
+
+alter table cliente add i_idade_cliente int;
+alter table cliente modify i_idade_cliente tinyint;
