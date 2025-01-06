7ab403a8d7f2ef347a7ffab689d5f6cb19bbffca
Bismarck <brunowasborn2rock@gmail.com>
2025-01-06 15:55:58 -0300

    insert and select

---
+++ Aula 9/aula9.sql
@@ -0,0 +1,19 @@
+select * from cliente;
+
+CREATE TABLE `cliente_aux` (
+  `i_cliente_cliente` int NOT NULL AUTO_INCREMENT,
+  `s_nomecli_cliente` varchar(30) DEFAULT NULL,
+  `s_cpf_cliente` varchar(50) NOT NULL,
+  `d_nasc_cliente` datetime DEFAULT NULL,
+  `i_tipo_cliente` int NOT NULL,
+  PRIMARY KEY (`i_cliente_cliente`),
+  KEY `fk_tipocliente` (`i_tipo_cliente`),
+  CONSTRAINT `fk_tipocliente_aux` FOREIGN KEY (`i_tipo_cliente`) REFERENCES `tipocliente` (`i_tipocliente_tipocliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+select * from cliente_aux;
+
+insert into cliente_aux(i_cliente_cliente, s_nomecli_cliente, s_cpf_cliente, d_nasc_cliente, i_tipo_cliente) 
+select i_cliente_cliente, s_nomecli_cliente, s_cpf_cliente, d_nasc_cliente, i_tipo_cliente from cliente;
+
+select * from cliente_aux;
