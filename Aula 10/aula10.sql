fc84ab6fb47fd7ea2c519ca25fc3770f792bd29f
Bismarck <brunowasborn2rock@gmail.com>
2025-01-07 16:50:58 -0300

    update

---
+++ Aula 10/aula10.sql
@@ -0,0 +1,15 @@
+update cliente set s_nomecli_cliente = 'Bruno B. Reichert', s_cpf_cliente = '86133802049 ' where i_cliente_cliente = 1;
+select * from cliente;
+
+update cliente set s_nomecli_cliente = 'Bruno', s_cpf_cliente = '12345678901' where i_cliente_cliente = 1;
+select * from cliente;
+
+update cliente set i_tipo_cliente = 1 WHERE i_cliente_cliente > 0;
+select * from cliente;
+
+delete from cliente where i_cliente_cliente > 0;
+select * from cliente;
+
+insert into cliente select * from cliente_aux;
+select * from cliente;
+
