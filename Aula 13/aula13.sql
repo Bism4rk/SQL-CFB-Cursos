2e8f8a4b33601c6d9af1294bdd256d82498f6aff
Bismarck <brunowasborn2rock@gmail.com>
2025-01-10 16:27:54 -0300

    aliases and distinct

---
+++ Aula 13/aula13.sql
@@ -0,0 +1,15 @@
+select i_cliente_cliente, upper(s_nomecli_cliente), i_tipo_cliente from cliente;
+
+select 
+	i_cliente_cliente as IDCliente, 
+    upper(s_nomecli_cliente) as NomeCliente, 
+    i_tipo_cliente as TipoCliente,
+    i_cliente_cliente * 2 as valorCliente
+from cliente;
+
+select 
+	tbc.i_cliente_cliente as IDCliente, 
+    upper(tbc.s_nomecli_cliente) as NomeCliente, 
+    tbc.i_tipo_cliente as TipoCliente,
+    tbc.i_cliente_cliente * 2 as valorCliente
+from cliente tbc;
